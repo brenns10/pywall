5368dfe2c48c98ee94c424babea88d92431d0196
Jeff Copeland Jr <jeffrey.copeland.jr@gmail.com>
2015-04-20 15:03:23 -0400

    Added logging to PrintRule

--- rules/print_rule.py
+++ rules/print_rule.py
@@ -2,14 +2,15 @@
 
 from __future__ import print_function
 from rules import register
+import logging
 
 
 class PrintRule(object):
 
     def __call__(self, packet):
         """Prints out packet information at the IP level."""
-        print(unicode(packet))
-        print(unicode(packet.get_payload()))
+        logging.error(unicode(packet))
+        logging.error(unicode(packet.get_payload()))
         return False
 
 
