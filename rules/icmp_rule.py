ccd445dac33be0648b2d90b06879cd9328ffeeea
Stephen Brennan <smb196@case.edu>
2015-04-21 12:56:41 -0400

    Added rules for matching any ICMP packet, or ICMP packets of a given type.

---
+++ rules/icmp_rule.py
@@ -0,0 +1,27 @@
+"""Rule matching ICMP packets."""
+
+from rules import Rule, register
+from packet import ICMPPacket
+
+
+class ICMPRule(Rule):
+
+    def filter_condition(self, pckt):
+        return type(pckt.get_payload()) is ICMPPacket
+
+
+class ICMPTypeCodeRule(ICMPRule):
+
+    def __init__(self, **kwargs):
+        ICMPRule.__init__(self, kwargs)
+        self.type = kwargs['type']
+        self.codes = kwargs['codes']
+
+    def filter_condition(self, pckt):
+        pl = pckt.get_payload()
+        return ICMPRule.filter_condition(self, pckt) and \
+            pl.type == self.type and pl.code in self.codes
+
+
+register(ICMPRule)
+register(ICMPTypeCodeRule)
