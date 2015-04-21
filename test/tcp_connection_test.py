2a590a9748a6a83018d30481cb5883dedbd4dbd4
Jeff Copeland Jr <jeffrey.copeland.jr@gmail.com>
2015-04-20 21:11:41 -0400

    Added expected number of connections to TCPServerProcess.

--- test/tcp_connection_test.py
+++ test/tcp_connection_test.py
@@ -6,10 +6,11 @@ import socket
 
 
 class TCPConnectionTest(PyWallTestCase):
-    def __init__(self, config_filename, port, client_timeout=1, server_timeout=5):
+    def __init__(self, config_filename, port, client_timeout=1, server_timeout=5, expected_num_connections=1):
         self.port = port
         self.timeout = client_timeout
-        PyWallTestCase.__init__(self, config_filename, TCPServerProcess(port, server_timeout))
+        PyWallTestCase.__init__(self, config_filename,
+                                TCPServerProcess(port, server_timeout, expected_num_connections))
 
     def client_request(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
--- test/tcp_server_process.py
+++ test/tcp_server_process.py
@@ -5,9 +5,10 @@ import select
 
 
 class TCPServerProcess(ServerProcess):
-    def __init__(self, port, timeout=5):
+    def __init__(self, port, timeout=5, expected_num_connections=1):
         self.port = port
         self.timeout = timeout
+        self.expected_num_connections = expected_num_connections
 
     def setup_socket(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -17,4 +18,5 @@ class TCPServerProcess(ServerProcess):
     def wait_socket(self):
         print('Waiting on server socket.')
         rlist, _, __ = select.select([self.sock], [], [], self.timeout)
-        return (len(rlist) >= 1)
+        return (len(rlist) == self.expected_num_connections)
+
