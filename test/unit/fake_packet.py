beb212f1d50e12d9b2066a459a68324bc8e3aec7
Andrew Mason <ajm188@case.edu>
2015-04-25 19:40:21 -0400

    change fake packet to support get_payload()

--- test/unit/fake_packet.py
+++ test/unit/fake_packet.py
@@ -3,8 +3,8 @@ class FakePacket(object):
     def __init__(self, **kwargs):
         self._src_ip = kwargs.get('src_ip')
         self._dst_ip = kwargs.get('dst_ip')
-        self._src_port = kwargs.get('src_port')
-        self._dst_port = kwargs.get('dst_port')
+        self._protocol = kwargs.get('protocol')
+        self._payload = FakeTransportPacket(**kwargs)
 
     def get_src_ip(self):
         return self._src_ip
@@ -12,9 +12,23 @@ class FakePacket(object):
     def get_dst_ip(self):
         return self._dst_ip
 
+    def get_protocol(self):
+        return self._protocol
+
+    def get_payload(self):
+        return self._payload
+
+
+class FakeTransportPacket(object):
+    
+    def __init__(self, **kwargs):
+        self._src_port = kwargs.get('src_port')
+        self._dst_port = kwargs.get('dst_port')
+
     def get_src_port(self):
         return self._src_port
 
     def get_dst_port(self):
         return self._dst_port
+
 
