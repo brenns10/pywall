0e7fe61e40f4afb731bf6c7a376bb2a2018cba7b
Jeff Copeland Jr <jeffrey.copeland.jr@gmail.com>
2015-04-26 23:01:19 -0400

    Pushed most config-based options down a level

--- test/acceptance/local/example_acceptance_test.py
+++ test/acceptance/local/example_acceptance_test.py
@@ -10,12 +10,12 @@ import socket
 
 
 class ExampleAcceptanceTestTCP(PyWallAcceptanceTestCase):
-    def __init__(self, config, host, key_file=None, remote_module='', remote_args=[]):
+    def __init__(self, config, key_file=None, remote_module='', remote_args=[]):
         listener = TCPListener(CONF['remote_host'], CONF['port'])
-        PyWallAcceptanceTestCase.__init__(self, config, host,
+        PyWallAcceptanceTestCase.__init__(self, config, '@'.join([CONF['user'], CONF['remote_host']]),
                                           remote_module=remote_module,
                                           remote_args=remote_args,
-                                          key_file=key_file,
+                                          key_file=CONF['key_file'],
                                           listener=listener)
 
     def run(self):
@@ -23,29 +23,24 @@ class ExampleAcceptanceTestTCP(PyWallAcceptanceTestCase):
 
 
 class ExampleAcceptanceTestUDP(PyWallAcceptanceTestCase):
-    def __init__(self, config, host, key_file=None, remote_module='', remote_args=[]):
+    def __init__(self, config, key_file=None, remote_module='', remote_args=[]):
         listener = UDPListener(CONF['port'])
-        PyWallAcceptanceTestCase.__init__(self, config, host,
+        PyWallAcceptanceTestCase.__init__(self, config, '@'.join([CONF['user'], CONF['remote_host']]),
                                           remote_module=remote_module,
                                           remote_args=remote_args,
-                                          key_file=key_file,
+                                          key_file=CONF['key_file'],
                                           listener=listener)
 
     def run(self):
         return bool(PyWallAcceptanceTestCase.run(self))
 
 
-
 tests = [
-    ('ExampleAcceptanceTest', ExampleAcceptanceTestTCP('example_config.json',
-                                                       '@'.join([CONF['user'], CONF['remote_host']]),
-                                                       key_file=CONF['key_file'],
-                                                       remote_module='example_test_remote.py',
-                                                       remote_args=[CONF['target_host'], str(CONF['port'])])),
-    ('ExampleAcceptanceTest', ExampleAcceptanceTestUDP('example_config.json',
-                                                       '@'.join([CONF['user'], CONF['remote_host']]),
-                                                       key_file=CONF['key_file'],
-                                                       remote_module='example_test_remote.py',
-                                                       remote_args=[CONF['target_host'], str(CONF['port']),'UDP'])),
-
+    ('ExampleAcceptanceTest (TCP)', ExampleAcceptanceTestTCP('example_config.json',
+                                                             remote_module='example_test_remote.py',
+                                                             remote_args=[CONF['target_host'], str(CONF['port'])])),
+    ('ExampleAcceptanceTest (UDP)', ExampleAcceptanceTestUDP('example_config.json',
+                                                             remote_module='example_test_remote.py',
+                                                             remote_args=[CONF['target_host'], str(CONF['port']),
+                                                                          'UDP'])),
 ]
