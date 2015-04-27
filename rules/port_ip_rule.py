1dbaf11ae8025fbf1ca0e0d91dca5a248df04d66
Stephen Brennan <smb196@case.edu>
2015-04-27 19:36:56 -0400

    Docstring'd rules/port_ip_rule.py.

--- rules/port_ip_rule.py
+++ rules/port_ip_rule.py
@@ -1,3 +1,5 @@
+"""Contains rules that act on a combination of IP and port."""
+
 from rules import register, SimpleRule
 from rules.port_filter import PortRangeRule
 from rules.ip_rules import SourceIPRule, DestinationIPRule
@@ -6,9 +8,13 @@ from rules.ip_rules import SourceIPRule, DestinationIPRule
 class IPPortRule(SimpleRule):
 
     def __init__(self, **kwargs):
+        """Creates a combination of rules."""
         SimpleRule.__init__(self, **kwargs)
+
+        # Create a port rule.
         self.port_rule = PortRangeRule(**kwargs)
 
+        # Create a source IP rule.
         source_args = kwargs.copy()
         source_ip = source_args.pop('src_ip', None)
         if source_ip:
@@ -17,6 +23,7 @@ class IPPortRule(SimpleRule):
         else:
             self.ip_src_rule = None
 
+        # Create a dest IP rule.
         dest_args = kwargs.copy()
         dest_ip = dest_args.pop('dst_ip', None)
         if dest_ip:
@@ -26,6 +33,7 @@ class IPPortRule(SimpleRule):
             self.ip_dst_rule = None
 
     def filter_condition(self, packet):
+        """Condition to filter on."""
         res = self.port_rule.filter_condition(packet)
         if self.ip_src_rule:
             res = res and self.ip_src_rule.filter_condition(packet)
