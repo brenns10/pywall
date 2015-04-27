a73b8687adcfcd3d1f8695625c5d5737c6b96d08
Jeff Copeland Jr <jeffrey.copeland.jr@gmail.com>
2015-04-27 18:28:59 -0400

    Made source port tests actually work

--- test/integration/port_filter_test.py
+++ test/integration/port_filter_test.py
@@ -8,32 +8,26 @@ import select
 
 tests = [
     # Tests src_port filtering
-    ('BlockLocalSrcHTTP', TCPConnectionTest('test/integration/block_local_src_web.json', 80, expected_num_connections=0)),
-    ('TCPUnregSrcPort1', TCPConnectionTest('test/integration/block_unreg_src_ports.json', 49151, expected_num_connections=0)),
-    ('TCPUnregSrcPort2', TCPConnectionTest('test/integration/block_unreg_src_ports.json', 49152, expected_num_connections=0)),
-    ('TCPUnregSrcPort3', TCPConnectionTest('test/integration/block_unreg_src_ports.json', 65535, expected_num_connections=0)),
-    ('BlockLocalSrcDNS', UDPConnectionTest('test/integration/block_local_src_web.json', 53, expected_num_connections=0)),
-    ('UDPUnregSrcPort1', UDPConnectionTest('test/integration/block_unreg_src_ports.json', 49151, expected_num_connections=0)),
-    ('UDPUnregSrcPort2', UDPConnectionTest('test/integration/block_unreg_src_ports.json', 49152, expected_num_connections=0)),
-    ('UDPUnregSrcPort3', UDPConnectionTest('test/integration/block_unreg_src_ports.json', 65535, expected_num_connections=0)),
+    ('TCPUnregSrcPort1', TCPConnectionTest('test/integration/block_unreg_src_ports.json', 49151, src_port=49251, expected_num_connections=0)),
+    ('TCPUnregSrcPort2', TCPConnectionTest('test/integration/block_unreg_src_ports.json', 49152, src_port=49252, expected_num_connections=0)),
+    ('TCPUnregSrcPort3', TCPConnectionTest('test/integration/block_unreg_src_ports.json', 65535, src_port=65435, expected_num_connections=0)),
+    ('UDPUnregSrcPort1', UDPConnectionTest('test/integration/block_unreg_src_ports.json', 49151, src_port=49251, expected_num_connections=0)),
+    ('UDPUnregSrcPort2', UDPConnectionTest('test/integration/block_unreg_src_ports.json', 49152, src_port=49252, expected_num_connections=0)),
+    ('UDPUnregSrcPort3', UDPConnectionTest('test/integration/block_unreg_src_ports.json', 65535, src_port=64535, expected_num_connections=0)),
 
     # Tests dst_port filters
-    ('BlockLocalDstHTTP', TCPConnectionTest('test/integration/block_local_dst_web.json', 80, expected_num_connections=0)),
     ('TCPUnregDstPort1', TCPConnectionTest('test/integration/block_unreg_dst_ports.json', 49151, expected_num_connections=0)),
     ('TCPUnregDstPort2', TCPConnectionTest('test/integration/block_unreg_dst_ports.json', 49152, expected_num_connections=0)),
     ('TCPUnregDstPort3', TCPConnectionTest('test/integration/block_unreg_dst_ports.json', 65535, expected_num_connections=0)),
-    ('BlockLocalDstDNS', UDPConnectionTest('test/integration/block_local_dst_web.json', 53, expected_num_connections=0)),
     ('UDPUnregDstPort1', UDPConnectionTest('test/integration/block_unreg_dst_ports.json', 49151, expected_num_connections=0)),
     ('UDPUnregDstPort2', UDPConnectionTest('test/integration/block_unreg_dst_ports.json', 49152, expected_num_connections=0)),
     ('UDPUnregDstPort3', UDPConnectionTest('test/integration/block_unreg_dst_ports.json', 65535, expected_num_connections=0)),
 
     # Tests combo dst/src_port filters
-    ('BlockLocalHTTP', TCPConnectionTest('test/integration/block_local_web.json', 80, expected_num_connections=0)),
-    ('TCPUnregPort1', TCPConnectionTest('test/integration/block_unreg_ports.json', 49151, expected_num_connections=0)),
-    ('TCPUnregPort2', TCPConnectionTest('test/integration/block_unreg_ports.json', 49152, expected_num_connections=0)),
-    ('TCPUnregPort3', TCPConnectionTest('test/integration/block_unreg_ports.json', 65535, expected_num_connections=0)),
-    ('BlockLocalDNS', UDPConnectionTest('test/integration/block_local_web.json', 53, expected_num_connections=0)),
-    ('UDPUnregPort1', UDPConnectionTest('test/integration/block_unreg_ports.json', 49151, expected_num_connections=0)),
-    ('UDPUnregPort2', UDPConnectionTest('test/integration/block_unreg_ports.json', 49152, expected_num_connections=0)),
-    ('UDPUnregPort3', UDPConnectionTest('test/integration/block_unreg_ports.json', 65535, expected_num_connections=0))
+    ('TCPUnregPort1', TCPConnectionTest('test/integration/block_unreg_ports.json', 49151, src_port=49251, expected_num_connections=0)),
+    ('TCPUnregPort2', TCPConnectionTest('test/integration/block_unreg_ports.json', 49152, src_port=49252, expected_num_connections=0)),
+    ('TCPUnregPort3', TCPConnectionTest('test/integration/block_unreg_ports.json', 65535, src_port=65435, expected_num_connections=0)),
+    ('UDPUnregPort1', UDPConnectionTest('test/integration/block_unreg_ports.json', 49151, src_port=49251, expected_num_connections=0)),
+    ('UDPUnregPort2', UDPConnectionTest('test/integration/block_unreg_ports.json', 49152, src_port=49252, expected_num_connections=0)),
+    ('UDPUnregPort3', UDPConnectionTest('test/integration/block_unreg_ports.json', 65535, src_port=65435, expected_num_connections=0)),
 ]
