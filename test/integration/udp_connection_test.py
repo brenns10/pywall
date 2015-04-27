7ff58398bd8965a4ce4e8da3bf67e566cc2cea2d
Jeff Copeland Jr <jeffrey.copeland.jr@gmail.com>
2015-04-27 18:28:59 -0400

    Made source port tests actually work

--- test/integration/udp_connection_test.py
+++ test/integration/udp_connection_test.py
@@ -6,14 +6,18 @@ import socket
 
 
 class UDPConnectionTest(PyWallTestCase):
-    def __init__(self, config_filename, port, client_timeout=1, server_timeout=5, expected_num_connections=1):
+    def __init__(self, config_filename, port, src_port=None, client_timeout=1,
+                 server_timeout=5, expected_num_connections=1):
         self.port = port
+        self.src_port = src_port
         self.timeout = client_timeout
         PyWallTestCase.__init__(self, config_filename,
                                 UDPServerProcess(port, server_timeout, expected_num_connections))
 
     def client_request(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        if self.src_port:
+            s.bind(('', self.src_port))
         s.sendto("hi!", ('localhost', self.port))
         s.close()
 
