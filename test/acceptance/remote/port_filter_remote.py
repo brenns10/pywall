f48061a1b2be539184e73bd62ff4b068e3b9fb5f
Jeff Copeland Jr <jeffrey.copeland.jr@gmail.com>
2015-04-27 01:20:28 -0400

    Added simple port filter tests

---
+++ test/acceptance/remote/port_filter_remote.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2
+from __future__ import print_function
+import socket
+import sys
+import time
+
+
+def rprint(msg):
+    """Print a message labelled from remote"""
+    print('[REMOTE] %s' % msg)
+
+
+    
