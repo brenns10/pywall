eb811ad4ffcdd5d0af161649662c7843ad86a6a8
Stephen Brennan <smb196@case.edu>
2015-04-27 19:12:26 -0400

    Docstring'd py_log.py.

--- py_log.py
+++ py_log.py
@@ -1,17 +1,33 @@
-"""Logging code goes here."""
+"""Contains code to setup logging, and to run the logger process.
+
+Sadly, logging in multiprocessing programs is not trivial.  Happily, Python
+provides wonderful logging utilities that make multiprocessing logging much
+nicer to setup.  We use these utilities here.  The external "logutils" module
+actually contains logging library items that are present in Python 3,
+backported to Python 2.
+
+"""
 
 import logging
 import time
 from logging import StreamHandler, FileHandler
+
 from logutils.queue import QueueHandler, QueueListener
 
 
 def _get_formatter():
+    """Creates a formatter with our specified format for log messages."""
     return logging.Formatter(fmt='[%(asctime)s][%(levelname)s] %(message)s')
 
 
 def initialize_logging(level, queue):
+    """Setup logging for a process.
 
+    Creates a base logger for pywall.  Installs a single handler, which will
+    send packets across a queue to the logger process.  This function should be
+    called by each of the three worker processes before they start.
+
+    """
     formatter = _get_formatter()
 
     logger = logging.getLogger('pywall')
@@ -25,7 +41,13 @@ def initialize_logging(level, queue):
 
 
 def log_server(level, queue, filename, mode='w'):
+    """Run the logging server.
 
+    This listens to the queue of log messages, and handles them using Python's
+    logging handlers.  It prints to stderr, as well as to a specified file, if
+    it is given.
+
+    """
     formatter = _get_formatter()
     handlers = []
 
