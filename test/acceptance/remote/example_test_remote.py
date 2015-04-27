4ab8959be8e54b37681b6ef771bbd89b6afa741b
Jeff Copeland Jr <jeffrey.copeland.jr@gmail.com>
2015-04-26 21:01:38 -0400

    Added better remote printout

--- test/acceptance/remote/example_test_remote.py
+++ test/acceptance/remote/example_test_remote.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     target_host = socket.gethostbyname(sys.argv[1])
     target_port = int(sys.argv[2]) if len(sys.argv) >= 3 else 9001
     target = (target_host, target_port)
-    print(target)
+    print('target: %s' % str(target))
     timeout = int(sys.argv[3]) if len(sys.argv) >= 4 else 5
 
     time.sleep(2)
