a991b10668ebacf53526ea6f062b6ccfb713733f
Stephen Brennan <smb196@case.edu>
2015-04-27 20:04:17 -0400

    Docstring'd rules/true_rule.py.

--- rules/true_rule.py
+++ rules/true_rule.py
@@ -1,6 +1,9 @@
+"""Rule that is always true."""
 from rules import register, SimpleRule
 
+
 class TrueRule(SimpleRule):
+    """Rule that is always true."""
 
     def filter_condition(self, pckt):
         return True
